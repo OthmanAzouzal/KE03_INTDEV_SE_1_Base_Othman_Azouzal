8e41916327d58c6c8bf99e1f31dd643326199f2c
Ottie3108@outlook.com <Ottie3108@outlook.com>
2025-05-23 12:46:00 +0200

    Fifth Commit

---
+++ KE03_INTDEV_SE_1_Base/Pages/Logout.cshtml.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KE03_INTDEV_SE_1_Base.Pages
+{
+    public class LogoutModel : PageModel
+    {
+        public IActionResult OnPost()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToPage("/Index");
+        }
+    }
+}
